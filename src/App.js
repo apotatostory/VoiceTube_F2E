b789fc417af73c944c626cf95ba225d42376ff89
apotatostory <apotatostory@gmail.com>
2018-05-21 22:48:22 +0800

    完成頁面設定規格

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Immutable  from 'immutable';
 import ShowView from './js/Component/ShowView';
 import Utils from './js/Component/Utils';
 
@@ -7,175 +6,135 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: '',
-            type: 'A',
+            data: '',   // 原始資料
+            sort: 'A',   // 初始排序
+            filter: 1, // 初始篩選
         };
     }
 
     componentDidMount() {
-        // 測試資料
-        // 原始資料
-        let sourceData = ([
-            {
-                "id": 52250,
-                "thumbnail": "https://cdn.voicetube.com/assets/thumbnails/QxjsWwgPjwM.jpg",
-                "title": "台北人英文真的超強？這部影片告訴你！(中英字幕) (Do They Speak English In Taiwan?)",
-                "views": 526816,
-                "collectCount": 200,
-                "duration": 20,
-                "publish": 1519880250,
-                "level": 1,
-                "captions": ["cht", "ja", "vi", "en"]
-            }, {
-                "id": 52251,
-                "thumbnail": "https://cdn.voicetube.com/assets/thumbnails/A9QDgSTVhyM_s.jpg",
-                "title": "寶寶失火了!! (THE BABIES ARE ON FIRE!)",
-                "views": 5268,
-                "collectCount": 180,
-                "duration": 60,
-                "publish": 1519880251,
-                "level": 1,
-                "captions": ["cht", "ja", "vi", "en"]
-            }, {
-                "id": 52252,
-                "thumbnail": "https://cdn.voicetube.com/assets/thumbnails/ab47XHidvwQ_s.jpg",
-                "title": "軟趴趴的烏賊是如何躲過或是擊退獵食者的？ ",
-                "views": 526899,
-                "collectCount": 299,
-                "duration": 319,
-                "publish": 1519880288,
-                "level": 1,
-                "captions": ["cht", "ja", "vi", "en"]
-            }, {
-                "id": 52253,
-                "thumbnail": "https://cdn.voicetube.com/assets/thumbnails/aIxfi8ikTSs_s.jpg",
-                "title": "坂本真綾 - Gravity (Maaya Sakamoto - Gravity)",
-                "views": 526700,
-                "collectCount": 250,
-                "duration": 500,
-                "publish": 1517880252,
-                "level": 1,
-                "captions": ["cht", "ja", "vi", "en"]
+        let success = value => {
+            if(value.status) {
+                // 初始化需要顯示的資料
+                let ctrlData = [];
+                value.data.forEach(x => ctrlData.push(x));
+
+                this.setState({
+                    data: value.data
+                })
             }
-        ]);
-
-        this.setState({
-            data: sourceData,
-        })
-
-        // 等待VoiceTube的API好打開註解 let success = value => {     this.setState({data:
-        // value}) }; let err = value => {     this.setState({data: 'error!!!!!!!'}) }
-        // // Utils.Ajax; Utils.Ajax('https://merik.voicetube.com/demo/data', 'GET', {},
-        // success.bind(this), err.bind(this));
+        };
+        let err = value => {
+            alert('ajax錯誤!!!');
+        }
+        Utils.Ajax('https://merik.voicetube.com/demo/data', 'GET', {}, success.bind(this), err.bind(this));
     }
 
-    /**
-     * 確認的事件
-     * 
-     * param type
-     */
-    handleClick = (type = 'A') => {
-        this.setState({
-            type: type,
-        })
+    sortClick = (type = 'A') => {
+        this.setState({sort: type})
     };
 
-    // filter
-    filterVideo = (value, type = 'A') => {
-        let result = {
-            'A': () => {value.sort((x, y) => x.publish > y.publish? 1 : -1)},
-            'B': () => {value.sort((x, y) => x.views > y.views? 1 : -1)},
-            'C': () => {value.sort((x, y) => x.collectCount > y.collectCount? 1 : -1)},
-            '1': () => {value = value.filter(() => true)},
-            '2': () => {value = value.filter(x => x.duration <= 240)},
-            '3': () => {value = value.filter(x => x.duration > 240 && x.duration <= 600)},
-            '4': () => {value = value.filter(x => x.duration > 600)}
+    filterClick = (type = 1) => {
+        this.setState({filter: type})
+    };
+
+    // 篩選
+    filterVideo = () => {
+        let sort = this.state.sort;
+        let filter = this.state.filter;
+        let data = this.state.data;
+        let result = [];
+
+        // 初始化顯示資料
+        if (data) {
+            data.forEach(x => result.push(x));
+        }
+        let filterUtil = {
+            'A': () => {result.sort((x, y) => x.publish > y.publish? 1 : -1)},
+            'B': () => {result.sort((x, y) => x.views > y.views? 1 : -1)},
+            'C': () => {result.sort((x, y) => x.collectCount > y.collectCount? 1 : -1)},
+            1: () => {result = result.filter(() => true)},
+            2: () => {result = result.filter(x => x.duration <= 240)},
+            3: () => {result = result.filter(x => x.duration > 240 && x.duration <= 600)},
+            4: () => {result = result.filter(x => x.duration > 600)}
         };
 
-        if(value) {
-            result[type]();
-        }
+        // 先篩選在排序
+        filterUtil[filter]();
+        filterUtil[sort]();
 
-        return value;
+        return result;
     };
 
     render() {
-        let data = this.state.data;
-        let showData;
 
-        if (data){
-            // 畫面顯示資料
-            let ctrlData = [];
-            data.forEach(x => ctrlData.push(x));
-            
-            // 篩選
-            showData = this.filterVideo(ctrlData, this.state.type);
-        }
-        
+        let showData = this.filterVideo();;
+
         return (
             <div className="container">
                 {/* 上層表單 */}
-                <nav className="navbar navbar-expand-sm">
+                <nav className="navbar navbar-expand-sm ">
                     <ul className="nav nav-pills" role="tablist">
                         <li className="nav-item">
-                            <div className="nav-link disabled">排序</div>
+                            <div className="nav-link disabled"><b>排序</b></div>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link active"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('A')}>發布時間</button>
+                                onClick={() => this.sortClick('A')}>發布時間</button>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('B')}>觀看次數</button>
+                                onClick={() => this.sortClick('B')}>觀看次數</button>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('C')}>收藏次數</button>
+                                onClick={() => this.sortClick('C')}>收藏次數</button>
                         </li>
                     </ul>
                     <ul className="nav nav-pills" role="tablist">
                         <li className="nav-item">
-                            <div className="nav-link disabled">長度</div>
+                            <div className="nav-link disabled"><b>長度</b></div>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link active"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('1')}>不限</button>
+                                onClick={() => this.filterClick(1)}>不限</button>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('2')}>4分鐘以下</button>
+                                onClick={() => this.filterClick(2)}>4分鐘以下</button>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('3')}>5-10分鐘</button>
+                                onClick={() => this.filterClick(3)}>5-10分鐘</button>
                         </li>
                         <li className="nav-item">
                             <button
                                 type="button"
                                 className="btn btn-light nav-link"
                                 data-toggle="pill"
-                                onClick={() => this.handleClick('4')}>超過10分鐘</button>
+                                onClick={() => this.filterClick(4)}>超過10分鐘</button>
                         </li>
                     </ul>
                 </nav>
+                {/* 篩選內容 */}
                 <div className="tab-content">
                     <div className="container tab-pane active">
                         <ShowView data={showData}/>
--- src/js/Component/ShowView.js
+++ src/js/Component/ShowView.js
@@ -11,26 +11,22 @@ class ShowView extends Component {
         let result = [];
 
         // 把array轉成實體
-        if (video && video.length != 0) {
-            console.log('有資料');
-
+        if (video && video.length !== 0) {
             video.forEach(element => {
                 result.push(
                     <div className="card">
                         <img className="card-img-top" src={element.thumbnail} alt="Card image"></img>
-                        <div className="card-body">
-                            <p className="card-text">{element.title}</p>
-                            <button type="button"></button>
-                        </div>
+                        <p className="card-text">{element.title}</p>
                     </div>
                 );
             })
             return (
-                <div>{result}</div>
+                <div className="card-columns">
+                    {result}
+                </div>
             );
 
         } else {
-            console.log('沒有資料');
             return (
                 <div>
                     <h2>沒有篩選結果</h2>
